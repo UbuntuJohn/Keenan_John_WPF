47a38378a157c8eedfe026dcf7fbf448ffefef73
John Keenan <john@new-host-2.home>
2013-11-07 15:27:55 -0500

    update JS file for Industry Expressions
    Just variables set

--- Expressions/Expressions_Industry/js/script.js
+++ Expressions/Expressions_Industry/js/script.js
@@ -1,1 +1,12 @@
-alert("This is in my JavaScript file.");
+/*
+
+Name: John Keenan
+Assignment: Expressions - Industry
+Date: November 7th, 2013
+
+*/
+
+console.log("Web Design Total Price Calculator");
+var amountPages = prompt("How many pages will your site have?");
+var pricePerPage = 50;
+var totalPrice = amountPages * pricePerPage;
