3310025df6265557ac59a60635cd98c0d9bfe141
John Keenan <john@new-host-2.home>
2013-11-07 15:18:21 -0500

    js Script for expressions_personal
    Commit for personal js script

--- Expressions/Expressions_Personal/js/script.js
+++ Expressions/Expressions_Personal/js/script.js
@@ -5,4 +5,15 @@ Assignment: Expressions
 Date: Nov. 7th, 2013
 
 */
+console.log("Car Price Calculator");
+var carPrice = 14000;
+var currentCash = prompt("How much money do you currently have?");
 
+if(currentCash < carPrice) {
+
+var cashLeft = carPrice - currentCash;
+
+console.log("You only need $" + cashLeft + " dollars to buy your new car.");
+} else {
+	console.log("Congrats! It appears you have enough to buy your new car!");
+}
