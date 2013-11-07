0ecf0032633f8b8e073cc77d476fc7c6bfdb7729
John Keenan <john@new-host-2.home>
2013-11-07 16:04:49 -0500

    Added new if statements
    added new if statements to complete the script

--- Expressions/Expressions_Wacky/js/script.js
+++ Expressions/Expressions_Wacky/js/script.js
@@ -20,6 +20,15 @@ console.log("You are a child... are you aware of COPA?");
 console.log("You are a teenager! Remember, school is important.");
 } else if(yourAge >=18 && yourAge <= 25) {
 console.log("You are a young adult... You're in the same age group of the developer.");
-} else if() {
-
+} else if(yourAge >= 26 && yourAge <= 39) {
+console.log("You are an adult... I bet you wished you were a child again.");
+} else if(yourAge >= 40 && yourAge <= 59) {
+console.log("You are middle aged (adult), sorry, but it's the truth!");
+} else if(yourAge >= 60 && yourAge <= 90) {
+console.log("You are a senior citizen. Note for developer: Never get old.");
+} else if(yourAge > 90) {
+console.log("You are a senior citizen. It is truely amazing that you are still alive.");
 }
+
+
+
