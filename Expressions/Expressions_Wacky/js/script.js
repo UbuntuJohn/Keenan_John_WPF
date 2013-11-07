a33d4e72f38cc25625ab5d404d77bc15c5db79c8
John Keenan <john@new-host-2.home>
2013-11-07 15:47:04 -0500

    Some if statements that deal with users age
    Calcuating age title

--- Expressions/Expressions_Wacky/js/script.js
+++ Expressions/Expressions_Wacky/js/script.js
@@ -5,3 +5,14 @@ Assignment: Expressions - Wacky
 Date: November 7th, 2013
 
 */
+
+console.log("Age Title Calculator");
+var yourAge = prompt("What is your age?");
+
+if(yourAge <= 0) {
+console.log("According to calculations... you aren't born yet");
+} else if(yourAge == 1 || yourAge == 2) {
+console.log("You are a toddler... how are you operating a computer?");
+} else if(yourAge >= 3 && yourAge <= 12) {
+console.log("You are a child... are you aware of COPA?");
+}
