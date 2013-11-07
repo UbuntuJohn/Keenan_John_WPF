1cad4408ca3c8c783540edb22bfa4e05a9564799
John Keenan <john@new-host-2.home>
2013-11-07 15:29:23 -0500

    console logging the data
    the data is now being console logged

--- Expressions/Expressions_Industry/js/script.js
+++ Expressions/Expressions_Industry/js/script.js
@@ -10,3 +10,4 @@ console.log("Web Design Total Price Calculator");
 var amountPages = prompt("How many pages will your site have?");
 var pricePerPage = 50;
 var totalPrice = amountPages * pricePerPage;
+console.log("Your price for this web design package will be $" + totalPrice + "!");
