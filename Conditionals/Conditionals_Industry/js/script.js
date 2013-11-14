f5f6d75cc8cfcddfd74bab7236ba93aa7fa78900
John Keenan <john@new-host-2.home>
2013-11-14 03:32:07 -0500

    expressions added for industry

--- Conditionals/Conditionals_Industry/js/script.js
+++ Conditionals/Conditionals_Industry/js/script.js
@@ -15,4 +15,20 @@ var totalWhitespace = spacingBetweenDivs * 3;
 var contentSpace = maxWidth - totalWhitespace;
 
 
+if(maxWidth === 960) {
+	if(totalWhitespace < 200 && totalWhitespace >= 0) {
+		console.log("You will have a total of " + contentSpace + " pixels to work with!");
+
+	} else if (totalWhitespace > 200) {
+		console.log("You may want to resize your whitespace pixel size.");
+
+	} else if(contentSpace > maxWidth) {
+		console.log("I see what you tried to do there... nice try though!");
+	}
+	
+} else {
+	console.log("Max width must be 960px");
+}
+
+
 
