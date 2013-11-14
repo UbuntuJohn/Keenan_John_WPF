dfbff84b3ee1848b8824074fafdb9216dd507fc1
John Keenan <john@new-host-2.home>
2013-11-14 03:27:50 -0500

    variables for personal

--- Conditionals/Conditionals_Personal/js/script.js
+++ Conditionals/Conditionals_Personal/js/script.js
@@ -7,6 +7,13 @@ Date: November 14th, 2013
 */
 
 
+var currentAmount = prompt("How many foot steps have you walked so far today?");
+
+currentAmount = Number(currentAmount);
+
+var minimumAmount = 2000;
+
+var stepsLeft = minimumAmount - currentAmount;
 
 
 
