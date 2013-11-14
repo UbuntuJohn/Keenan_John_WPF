3ec9be2f74486d9a06482ea84c4b7ea017340574
John Keenan <john@new-host-2.home>
2013-11-14 03:35:57 -0500

    code with ternary with wacky

--- Conditionals/Conditionals_Wacky/js/script.js
+++ Conditionals/Conditionals_Wacky/js/script.js
@@ -6,3 +6,11 @@ Date: November 14th, 2013
 
 */
 
+/* ------------ Bad luck cat ------------- */
+
+alert("You now have a cat!");
+
+var catColor = prompt("What color is your cat?");
+
+(catColor != "black") ? console.log("Your cat doesn't superstitiously bring bad luck.") : console.log("Your cat superstitiously brings bad luck!");
+
