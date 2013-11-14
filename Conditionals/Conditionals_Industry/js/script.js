245d77c230dd0c34998b069a6c9a62e3f93b2726
John Keenan <john@new-host-2.home>
2013-11-14 03:31:13 -0500

    variables created for Industry

--- Conditionals/Conditionals_Industry/js/script.js
+++ Conditionals/Conditionals_Industry/js/script.js
@@ -6,4 +6,13 @@ Date: November 14th, 2013
 
 */
 
+var maxWidth = 960;
+
+var spacingBetweenDivs  = prompt("How many pixels will between your two paneled website? (Example: 15)");
+
+var totalWhitespace = spacingBetweenDivs * 3;
+
+var contentSpace = maxWidth - totalWhitespace;
+
+
 
