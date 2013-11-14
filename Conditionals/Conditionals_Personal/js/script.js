04f857ded786199454fe9445b1f436bb8ce03b06
John Keenan <john@new-host-2.home>
2013-11-14 03:28:45 -0500

    expressions added for personal

--- Conditionals/Conditionals_Personal/js/script.js
+++ Conditionals/Conditionals_Personal/js/script.js
@@ -15,6 +15,19 @@ var minimumAmount = 2000;
 
 var stepsLeft = minimumAmount - currentAmount;
 
+if(currentAmount <= 0) {
+	currentAmount = 0;
+	console.log("You haven't started your workout yet! You should get right to it!");
+
+} else if(currentAmount < minimumAmount) {
+	console.log("You have a total of " + stepsLeft + " steps to walk until the minimum of " + minimumAmount + " steps.");
+
+} else if(currentAmount > minimumAmount) {
+	console.log("Feel free to stop at any time! You have reached your minimum steps for today!");
+
+} else if(currentAmount === minimumAmount) {
+	console.log("Rght on the dot! You've reach the minimum requirement for workout today!");
+} 
 
 
 
