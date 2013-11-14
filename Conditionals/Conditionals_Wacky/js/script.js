923e41147e682a7b53fe71e3017a9a21235dd05c
John Keenan <john@new-host-2.home>
2013-11-14 03:36:16 -0500

    comments added in wacky

--- Conditionals/Conditionals_Wacky/js/script.js
+++ Conditionals/Conditionals_Wacky/js/script.js
@@ -7,10 +7,9 @@ Date: November 14th, 2013
 */
 
 /* ------------ Bad luck cat ------------- */
-
+//Tells the visitor they now have a cat
 alert("You now have a cat!");
-
+//Asks the visitors what color there cat is
 var catColor = prompt("What color is your cat?");
-
+//The ternaries that tells the visitors if their cat brings bad luck
 (catColor != "black") ? console.log("Your cat doesn't superstitiously bring bad luck.") : console.log("Your cat superstitiously brings bad luck!");
-
