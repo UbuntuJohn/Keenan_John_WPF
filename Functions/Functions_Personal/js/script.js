38c6d254a5a7c0cae297ebd8bdbc275ab028b16a
John Keenan <john@new-host-2.home>
2013-11-15 12:41:39 -0500

    Code Completion for Function Person

--- Functions/Functions_Personal/js/script.js
+++ Functions/Functions_Personal/js/script.js
@@ -5,3 +5,26 @@ Assignment: Functions Personal
 Date: November 15, 2013
 
 */
+
+/* -------- Drinking Water --------- */
+
+var amountDrank = prompt("How many bottles of water did you drink today?");
+
+function drinkDrunk(bottles) {
+
+	var drinkMinimum = 3;
+	var totalLeft = drinkMinimum - amountDrank;
+	var totalMet;
+
+	if(amountDrank < drinkMinimum) {
+		totalMet = "No";
+		return totalMet;
+	} else if(amountDrank > drinkMinimum) {
+		totalMet = "Yes";
+		return totalMet;
+	}
+
+}
+
+var drank = drinkDrunk(amountDrank);
+console.log("Daily requirement met: " + drank);
