9414c03062167a553cd11d1b4300c04b6e9795c4
John Keenan <john@new-host-2.home>
2013-11-15 13:39:58 -0500

    Industry Script completed

--- Functions/Functions_Industry/js/script.js
+++ Functions/Functions_Industry/js/script.js
@@ -8,12 +8,21 @@ Date: November 15, 2013
 
 /* --------- Photos per Page + Pages ---------- */
 
+
 var totalPhotos = prompt("How many photos do you have?");
 var photosPerPage = prompt("How many photos will you have displaying on each page?");
 
 var photoGalleryPages = function(photosPerPage, totalPhotos) {
 
-	
+	var pagesAmount = photosPerPage / totalPhotos;
 
-
+	return pagesAmount;
 }
+
+
+var pages = photoGalleryPages(totalPhotos,photosPerPage);
+
+(pages < 1) ? pages = 1 : false;
+
+var roundedPages = Math.ceil(pages);
+console.log("There will be a total of " + roundedPages + " page(s) in your photo gallery!");
