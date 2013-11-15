73c769e3cad9faa5722585873a3a9b91b5cfc136
John Keenan <john@new-host-2.home>
2013-11-15 08:31:46 -0500

    circumference completed

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -6,3 +6,17 @@ Date: November 15, 2013
 
 */
 
+/* --------- Circumference ---------- */
+
+function circumference(radius) {
+
+	var pi = 3.14;
+	var equation = 2 * pi * radius;
+	return equation;
+
+}
+
+var c = circumference(10);
+console.log("The circumference of the circle is " + c);
+
+/* --------- Stung! ---------- */
