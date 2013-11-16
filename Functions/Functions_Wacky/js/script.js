63c6636fba81ca5f5df9d38583e5b2e12173029b
John Keenan <john@new-host-2.home>
2013-11-15 23:56:53 -0500

    Comment: explains what function does

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -11,6 +11,7 @@ Date: November 15, 2013
 //Asks the user how many licks they have done so far
 var tootsieLicks = prompt("How many licks have you performed so far?");
 
+//function that calculates percentage
 function getLicksPercent(tootsieLicks) {
 	var purdueStudy = 364;
 
