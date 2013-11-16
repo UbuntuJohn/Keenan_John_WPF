c70f035d9baa0033e4de6a6c24de65ff2f6220ad
John Keenan <john@new-host-2.home>
2013-11-15 23:55:15 -0500

    Functions Wacky script completed

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -6,10 +6,23 @@ Date: November 15, 2013
 
 */
 
-/* -------- Tootsie Pop Licks Percent --------- */
+/* -------- Tootsie Pop Licks Percent (According to Purdue University) --------- */
 
 var tootsieLicks = prompt("How many licks have you performed so far?");
 
-function getLicksPercent() {
-	
+function getLicksPercent(tootsieLicks) {
+	var purdueStudy = 364;
+
+	if(tootsieLicks > purdueStudy) {
+		console.log("According to Purdue University, you should already be done... Maybe look at one of the other studies?");
+		die();
+	} else {
+
+	var equation = tootsieLicks / purdueStudy * 100;
+	return equation;
+
+	} 
 }
+
+var percentage = getLicksPercent(tootsieLicks);
+console.log("You are " + percentage + "% to reaching the middle!");
